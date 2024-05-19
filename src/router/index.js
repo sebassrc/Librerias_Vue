43f11659f9a23e058a386877b286b5871c8f0745
Sebastian Rodriguez Camilo <segarodriguezcamilo@gmail.com>
2024-05-18 22:40:00 -0500

    router index v2

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,25 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
 import Clientes from '../views/Clientes.vue';
 import Libros from '../views/Libros.vue';
 import Ventas from '../views/Ventas.vue';
 import Proveedores from '../views/Proveedores.vue';
 import Eventos from '../views/Eventos.vue';
+
+import EditarCliente from '../components/Cliente/EditarCliente.vue';
+import NewCliente from '../components/Cliente/NewCliente.vue';
+
+import EditarLibro from '../components/Libro/EditarLibro.vue';
+import NewLibro from '../components/Libro/NewLibro.vue';
+
+import EditarVenta from '../components/Venta/EditarVenta.vue';
+import NewVenta from '../components/Venta/NewVenta.vue';
+
+import EditarProveedor from '../components/Proveedor/EditarProveedor.vue';
+import NewProveedor from '../components/Proveedor/NewProveedor.vue';
+
+import EditarEvento from '../components/Evento/EditarEvento.vue';
+import NewEvento from '../components/Evento/NewEvento.vue';
 
 const routes = [
   {
@@ -18,9 +33,29 @@ const routes = [
     component: Clientes
   },
   {
+    path: '/editar-cliente/:id',
+    name: 'EditarCliente',
+    component: EditarCliente
+  },
+  {
+    path: '/add-cliente/',
+    name: 'NewCliente',
+    component: NewCliente
+  },
+  {
     path: '/libros',
     name: 'libros',
     component: Libros
+  },
+  {
+    path: '/editar-libro/:id',
+    name: 'EditarLibro',
+    component: EditarLibro
+  },
+  {
+    path: '/add-libro/',
+    name: 'NewLibro',
+    component: NewLibro
   },
   {
     path: '/ventas',
@@ -28,14 +63,44 @@ const routes = [
     component: Ventas
   },
   {
+    path: '/editar-venta/:id',
+    name: 'EditarVenta',
+    component: EditarVenta
+  },
+  {
+    path: '/add-venta/',
+    name: 'NewVenta',
+    component: NewVenta
+  },
+  {
     path: '/proveedores',
     name: 'proveedores',
     component: Proveedores
   },
   {
+    path: '/editar-proveedor/:id',
+    name: 'EditarProveedor',
+    component: EditarProveedor
+  },
+  {
+    path: '/add-proveedor/',
+    name: 'NewProveedor',
+    component: NewProveedor
+  },
+  {
     path: '/eventos',
     name: 'eventos',
     component: Eventos
+  },
+  {
+    path: '/editar-evento/:id',
+    name: 'EditarEvento',
+    component: EditarEvento
+  },
+  {
+    path: '/add-evento/',
+    name: 'NewEvento',
+    component: NewEvento
   },
   {
     path: '/about',
@@ -45,11 +110,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
 
-export default router
+export default router;
